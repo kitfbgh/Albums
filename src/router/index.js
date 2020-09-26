6842b92c79b1d4c745675d7f0bdc843954e02ff9
kitfbgh <64077609+kitfbgh@users.noreply.github.com>
2020-09-26 12:27:22 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import SignUpPage from '../views/SignUpPage.vue';
+import AlbumsDetailPage from '../views/AlbumsDetailPage.vue';
+import AlbumsPage from '../views/AlbumsPage.vue';
+import { Auth } from 'aws-amplify';
 
 Vue.use(VueRouter)
 
@@ -11,19 +15,42 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: "/signup",
+    name: "SignUpPage",
+    component: SignUpPage
+
+  },
+  {
+    path: "/album/:id",
+    name: "AlbumsDetailPage",
+    component: AlbumsDetailPage,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: "/albums",
+    name: "AlbumsPage",
+    component: AlbumsPage,
+    meta: { requiresAuth: true }
+
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  const isAuthenticated = await Auth.currentUserInfo();
+
+  if (requiresAuth && !isAuthenticated) {
+    next("/");
+  } else {
+    next()
+  }
+
 })
 
 export default router
